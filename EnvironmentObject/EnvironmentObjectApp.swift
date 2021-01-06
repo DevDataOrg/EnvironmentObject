fddc53234e09ddb3d260a9643bf4d24b6487e067
DevDataOrg <48391348+DevDataOrg@users.noreply.github.com>
2021-01-06 11:11:52 +0300

    Initial Commit

---
+++ EnvironmentObject/EnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjectApp.swift
+//  EnvironmentObject
+//
+//  Created by Artyom Romanchuk on 06.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
