7621aba3364d585e55fdf0342f6c61ee7db8a49b
DevDataOrg <48391348+DevDataOrg@users.noreply.github.com>
2021-01-06 19:26:40 +0300

    Fix preview Canvas  add  ContentView().environmentObject(UserSettings())

--- EnvironmentObject/ContentView.swift
+++ EnvironmentObject/ContentView.swift
@@ -29,6 +29,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(UserSettings())
     }
 }
